0af78cb09dbd97dc9484a992c3e218bd17d33dd9
ohyearim <91578173+iepppop@users.noreply.github.com>
2023-12-20 02:28:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    var answer = [num];
+    let val = num;
+
+    while (val !== 1) {
+        if (val % 2 === 0) {
+            val = val / 2;
+        } else {
+            val = 3 * val + 1;
+        }
+        answer.push(val);
+    }
+
+    return answer.length < 500 ? answer.length - 1 : -1;
+}
