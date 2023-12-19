7e34b8c32ce44e54f4f7cce22d26371a859e9606
ohyearim <91578173+iepppop@users.noreply.github.com>
2023-12-20 02:33:35 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,14 +1,15 @@
-function solution(n) {
-    var answer = [n];
-    let val= n % 2===0 ? n/2 : 3*n+1;
-    while(val !== 1) {
-        answer.push(val)
-        if(val%2 === 0) {
-            val/=2
+function solution(num) {
+    var answer = [num];
+    let val = num;
+
+    while (val !== 1) {
+        if (val % 2 === 0) {
+            val = val / 2;
         } else {
-            val = 3 * val + 1
+            val = 3 * val + 1;
         }
+        answer.push(val);
     }
-    answer.push(1)
+
     return answer;
 }
