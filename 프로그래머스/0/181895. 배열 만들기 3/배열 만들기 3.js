e55c7b3df71a91f1982a333ac3129189a292f97c
ohyearim <91578173+iepppop@users.noreply.github.com>
2024-03-20 23:51:37 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.44 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,7 +1,4 @@
 function solution(arr, intervals) {
-    var answer = 0;
-    const answer1 = arr.slice(intervals[0][0],intervals[0][1]+ 1).join(" ").split(" ").map(item => parseInt(item))
-    const answer2 =  arr.slice(intervals[1][0],intervals[1][1] + 1).join(" ").split(" ").map(item => parseInt(item))
-    answer = [...answer1,...answer2]
-    return answer;
+    const [[a,b],[c,d]] = intervals;
+    return [...arr.slice(a,b + 1),...arr.slice(c,d + 1)]
 }
